fba97f324e0a54704f902ec3c7704e7e7c97ca2c
rishithSadashiv <rishith.1si16ec078@gmail.com>
2020-12-11 19:06:06 +0530

    microservice3 complete

---
+++ Microservice1/Controllers/CompanyController.cs
@@ -0,0 +1,100 @@
+﻿using Microservice3.Domain.Contracts;
+using Microservice3.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        readonly ICompanyService companyService;
+        public CompanyController(ICompanyService companyService)
+        {
+            this.companyService = companyService;
+        }
+
+        [HttpGet]
+        [Route("{id}/company")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(CompanyDto))]
+        public IActionResult GetCompany(int id)
+        {
+            var Obj = companyService.GetCompany(id);
+            if (Obj == null)
+                return NotFound();
+
+            return Ok(Obj);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(201)]
+        public IActionResult UpdateCompany(CompanyDto obj)
+        {
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (obj == null)
+                return BadRequest("Product is required");
+
+            var prod = companyService.GetCompany(obj.CompanyID);
+
+            if (prod == null)
+            {
+                companyService.AddCompany(obj);
+                return StatusCode(201);
+            }
+            var result = companyService.UpdateCompany(obj);
+            if (result)
+                return Ok();
+            else
+                return BadRequest("Update failed");
+
+
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201)]
+        public IActionResult AddCompany(CompanyDto company)
+        {
+            if (ModelState.IsValid == false)
+                return BadRequest(ModelState);
+
+            var result = companyService.AddCompany(company);
+            if (!result)
+                return BadRequest("Error saving products");
+
+            //return CreatedAtRoute("GetProductById", new { id = obj.ID });
+            return StatusCode(201);
+        }
+
+        [HttpGet]
+        [Route("{sector}/sectorprice")]
+        public IActionResult getSectorPrice(string sector)
+        {
+            int data = companyService.getSectorPrice(sector);
+            return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("{sector}/companies")] //standard way of defining the request
+        public IActionResult GetCompaniesInSector(string sector)
+        {
+            var data = companyService.GetCompaniesInSector(sector);
+            return Ok(data);
+        }
+
+
+    }
+}
--- Microservice1/Controllers/SectorsController.cs
+++ Microservice1/Controllers/SectorsController.cs
@@ -17,37 +17,21 @@ namespace Microservice3.Controllers
     public class SectorsController : ControllerBase
     {
         readonly ISectorService sectorService;
-        readonly ICompanyService companyService;
-        public SectorsController(ISectorService sectorService, ICompanyService companyService)
+        public SectorsController(ISectorService sectorService)
         {
             this.sectorService = sectorService;
-            this.companyService = companyService;
         }
+
         [HttpGet]
-        public IActionResult getSectors()
+        public IActionResult GetSectors()
         {
             var data = sectorService.getSectors();
             return Ok(data);
         }
-        [HttpGet]
-        [Route("{sector}/companies")] //standard way of defining the request
-        public IActionResult GetCompaniesInSector(string sector)
-        {
-            var data = companyService.GetCompaniesInSector(sector);
-            return Ok(data);
-        }
-        [HttpGet]
-        [Route("getSectorPrice/{sector}")]
-        public IActionResult getSectorPrice(string sector)
-        {
-            int data = companyService.getSectorPrice(sector);
-            return Ok(data);
-        }
-
+ 
         [HttpPost]
         [ProducesResponseType(400)]
         [ProducesResponseType(201)]
-        [Route("addsector")]
         public IActionResult AddSector(SectorDto sector)
         {
             if (ModelState.IsValid == false)
@@ -60,71 +44,6 @@ namespace Microservice3.Controllers
             //return CreatedAtRoute("GetProductById", new { id = obj.ID });
             return StatusCode(201);
         }
-
-
-        [HttpPost]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(201)]
-        [Route("addcompany")]
-        public IActionResult AddCompany(CompanyDto company)
-        {
-            if (ModelState.IsValid == false)
-                return BadRequest(ModelState);
-
-            var result = companyService.AddCompany(company);
-            if (!result)
-                return BadRequest("Error saving products");
-
-            //return CreatedAtRoute("GetProductById", new { id = obj.ID });
-            return StatusCode(201);
-        }
-
-        [HttpPut]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(201)]
-        [Route("updatecompany")]
-        public IActionResult UpdateCompany(CompanyDto obj)
-        {
-            
-                if (!ModelState.IsValid)
-                    return BadRequest(ModelState);
-
-                if (obj == null)
-                    return BadRequest("Product is required");
-
-                var prod = companyService.GetCompany(obj.CompanyID);
-
-            if (prod == null)
-            {
-                companyService.AddCompany(obj);
-                return StatusCode(201);
-            }
-                var result = companyService.UpdateCompany(obj);
-                if (result)
-                    return Ok();
-                else
-                    return BadRequest("Update failed");
-            
-            
-        }
-
-
-        [HttpGet]
-        [Route("GetCompanyById/{id}")]
-        [ProducesResponseType(404)]
-        [ProducesResponseType(200, Type = typeof(CompanyDto))]
-        public IActionResult GetCompany(int id)
-        {
-            var Obj = companyService.GetCompany(id);
-            if (Obj == null)
-                return NotFound();
-
-            return Ok(Obj);
-        }
-
-
 
 
     }
